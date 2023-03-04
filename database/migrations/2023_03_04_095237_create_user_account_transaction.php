bd90e2710d5a68ec1e7fba6d25c72aa8076b89e0
Basil M Mathew <basilmmathew17@gmail.com>
2023-03-04 15:53:33 +0530

    New app Banking

---
+++ database/migrations/2023_03_04_095237_create_user_account_transaction.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_account_transaction', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_account_id');
+            $table->tinyInteger('transaction_type');
+            $table->float('amount');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_account_transaction');
+    }
+};
